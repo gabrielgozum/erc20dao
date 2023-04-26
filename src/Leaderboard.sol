bf57448724c6137e6dcd8c7d2fe9011c86a869ff
Gabriel Gozum <gab@gabBook.local>
2023-04-26 17:31:24 -0500

    changes

---
+++ src/Leaderboard.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+contract Leaderboard is ERC20{
+    // create address for contract owner
+    address private owner;
+
+    constructor () ERC20 ("Leaderboard", "LDB"){
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) public virtual {
+        // assert sender address is the contract owner
+        assert(msg.sender == owner);
+        _mint(to,amount);
+    }
+
+    function burn(address form, uint amount) public virtual {
+        _burn(form, amount);
+    }
+}
